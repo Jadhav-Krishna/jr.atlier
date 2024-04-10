1565c88df26ccb483a145e6ec97914b30cc2d9ce
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-04-11 02:58:03 +0530

    responsive

--- script.js
+++ script.js
@@ -135,16 +135,79 @@ function anime() {
 }
 anime();
 
-function sheryx(){
-  Shery.hoverWithMediaCircle("#img-hover1","#img-hover2","#img-hove3","#img-hover4",/* Element to target.*/ {
-    images: ["img1.webp", "img2.webp", "img3.webp","img4.webp"],
-  });
-  // Shery.mouseFollower({
-  //   //Parameters are optional.
-  //   skew: true,
-  //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  //   duration: 1,
-  // });
-  Shery.mouseFollower();
+function page10(){
+  gsap.from("#circle1 img",{
+    scale:0,
+    duration:0.5,
+    ease:"power1.inOut",
+  })
+  let icircle = document.querySelector("#icircle");
+  let name = document.querySelector("#name");
+  let profile = document.querySelector("#profile");
+  let job = document.querySelector("#job");
+  let para = document.querySelector("para");
+  document.querySelector("#circlee1").addEventListener("click", function() {
+    icircle.setAttribute("src","./p1.webp");
+    name.textContent="LEOPOLD DURAND";
+    profile.textContent="TALENTED CARPENTER AND CRAFTSMAN";
+    job.textContent="FD FRAMEWORK & MODULEO";
+    para.textContent= "« I am delighted with the collaboration started with Julie for several years already. Very competent, at’ constantly listens to our needs and wishes. By its role of...";
+  })
+  
+  document.querySelector("#circlee2").addEventListener("click", function() {
+    icircle.setAttribute("src","./p2.webp");
+    name.textContent="LIDDARY KHAM'S";
+    profile.textContent="MARKETING COMMUNICATION MANAGER";
+    job.textContent="OFFICE CONCEPT & ARTECK";
+    para.textContent= "« With the accompaniment of Julie, our brand image finally reflects our adn and highlights our competitive advantages. New visual identity, new communication media...";
+  })
+
+  document.querySelector("#circlee3").addEventListener("click", function() {
+    icircle.setAttribute("src","./p3.webp");
+    name.textContent="JOS DE KONING'S";
+    profile.textContent="FOUNDER & DIRECTOR";
+    job.textContent="FRUITS AT HOME";
+    para.textContent= "« The meeting with Julie, started in 2017, around my need to renew the’ identity of our brand « Fruits at Home » and the packaging for the range of products Citron Caviar. La...";
+  })
+
+  document.querySelector("#circlee4").addEventListener("click", function() {
+    icircle.setAttribute("src","./p44.webp");
+    name.textContent="JESSICA ROLLAND";
+    profile.textContent="FOUNDER OF 1000 IDEAS PER MINUTE";
+    job.textContent="AGENCY SOMEDAY";
+    para.textContent= "« Julie accompanied us in the development of our visual identity and guided us to proposals both creative and completely adapted to our universe. Beyond...";
+  })
+
+  document.querySelector("#circlee5").addEventListener("click", function() {
+    icircle.setAttribute("src","./p55.webp");
+    name.textContent="ETIENNE ROZAND";
+    profile.textContent="CHAIR";
+    job.textContent="MIAM FAMILY'S";
+    para.textContent= "<< Excellent experience on our collaboration with Julie. Very happy for our brand Lipopette rendering around our visual identity and that of our sauces, it stuck...";
+  })
+
+  document.querySelector("#circlee6").addEventListener("click", function() {
+    icircle.setAttribute("src","./p6.webp");
+    name.textContent="BRICE ETIENNE";
+    profile.textContent="FOUNDER & DIRECTOR";
+    job.textContent="VINYL BAR AND KEY ARTISTS";
+    para.textContent= "“Julie Rignanese accompanied us in the creation and definition of the’ visual identity of our new concept of Vinyl Bar. She knew how to define our expectations, refocus our...";
+  })
+
+  document.querySelector("#circlee7").addEventListener("click", function() {
+    icircle.setAttribute("src","./p7.webp");
+    name.textContent="LAURENCE CORTINOVIS";
+    profile.textContent="DIRECTOR, HRD TIME-SHARE EXPERT";
+    job.textContent="COREAS";
+    para.textContent= "« Following my move to Lyon, I’ asked Julie to me’aider to rethink my entire communication strategy. She was great and went much further than...";
+  })
+
+  document.querySelector("#circlee8").addEventListener("click", function() {
+    icircle.setAttribute("src","./p8.webp");
+    name.textContent="OLIVIER HERLENT";
+    profile.textContent="PASSIONATE DIRECTOR AND ENTREPRENEUR";
+    job.textContent="BEST-ARTISAN.COM";
+    para.textContent= "« We entrusted Julie with the complete redesign of our graphic identity and its declination on our website.Beautiful remote collaboration : Julie is very pro, always good...";
+  })
 }
-sheryx();
+page10();
